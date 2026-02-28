c903a68bfc7e3192ebe8ce6e133ea551b4f7aaf9
flankel <flankel1919@gmail.com>
2026-02-28 11:31:13 +0900

    Update loadHeader.js

--- js/loadHeader.js
+++ js/loadHeader.js
@@ -1,1 +1,17 @@
-
+    fetch("header.html")
+      .then(res => res.text())
+      .then(data => {
+        document.getElementById("header").innerHTML = data;
+    
+        // ★ 現在のページを取得
+        const current = location.pathname.split("/").pop();
+    
+        // ★ nav-link を全部チェック
+        document.querySelectorAll(".nav-link").forEach(link => {
+          const href = link.getAttribute("href");
+    
+          if (href === current || (current === "" && href === "index.html")) {
+            link.classList.add("active");
+          }
+        });
+      });
