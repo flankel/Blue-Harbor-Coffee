6e0af3557e8a3db80ce9316a3d09f4b9fbd7955b
flankel <flankel1919@gmail.com>
2026-03-09 11:00:39 +0900

    Update confirm.js

--- js/confirm.js
+++ js/confirm.js
@@ -15,12 +15,13 @@ serverTimestamp
 // ==============================
 
 const firebaseConfig = {
-apiKey: "YOUR_API_KEY",
-authDomain: "YOUR_PROJECT.firebaseapp.com",
-projectId: "YOUR_PROJECT_ID",
-storageBucket: "YOUR_PROJECT.appspot.com",
-messagingSenderId: "XXXX",
-appId: "XXXX"
+  apiKey: "AIzaSyBlBC7PgW3aCvulWTJu3YMs9HPRydRdjY0",
+  authDomain: "blue-harbor-takeout.firebaseapp.com",
+  projectId: "blue-harbor-takeout",
+  storageBucket: "blue-harbor-takeout.firebasestorage.app",
+  messagingSenderId: "687997239074",
+  appId: "1:687997239074:web:d29a92a47c69e2f67aaf7b",
+  measurementId: "G-3S75RH1N5G"
 };
 
 const app = initializeApp(firebaseConfig);
