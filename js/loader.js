d65db8939c7fa153de37f94471959e426136c7c0
flankel <flankel1919@gmail.com>
2026-04-23 18:41:50 +0900

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -1,25 +1,27 @@
 export function initLoader() {
-  console.log("✅ initLoader CALLED");
+  console.log("1. initLoader start");
 
   const root = document.getElementById("loader-root");
-  console.log("loader-root:", root);
+  console.log("2. root =", root);
 
   fetch("./loader.html")
     .then(res => {
-      console.log("loader.html status:", res.status);
+      console.log("3. fetch status =", res.status);
       return res.text();
     })
     .then(html => {
-      console.log("loader.html loaded");
+      console.log("4. html loaded");
+
+      console.log("5. html =", html);
 
       root.innerHTML = html;
 
-      console.log("HTML injected");
+      console.log("6. injected");
 
       const percent = document.getElementById("percent");
-      console.log("percent element:", percent);
+      console.log("7. percent =", percent);
     })
     .catch(err => {
-      console.error("❌ loader error:", err);
+      console.error("❌ ERROR:", err);
     });
 }
