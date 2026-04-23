2161f35fbe68a84af009ae9bb3c3693b78e81c5f
flankel <flankel1919@gmail.com>
2026-04-23 12:18:14 +0900

    Create loader.js

---
+++ js/loader.js
@@ -0,0 +1,30 @@
+// /js/loader.js
+
+async function loadLoader() {
+  const res = await fetch("/components/loader.html");
+  const html = await res.text();
+  document.getElementById("loader-root").innerHTML = html;
+}
+
+function initLoader() {
+  window.addEventListener("load", () => {
+
+    const loader = document.getElementById("loader");
+    const body = document.getElementById("body");
+
+    setTimeout(() => {
+
+      loader.style.opacity = "0";
+
+      setTimeout(() => {
+        loader.style.display = "none";
+        body.classList.remove("opacity-0");
+      }, 700);
+
+    }, 600);
+
+  });
+}
+
+// 実行
+loadLoader().then(initLoader);
